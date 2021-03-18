7aff04a3851f4e9ba5e1eeafe96f0ac6c968581a
turismo53 <turismo53@gmail.com>
2021-03-18 13:38:49 +0200

    java basics

--- app/src/main/java/com/example/training/MainActivity.java
+++ app/src/main/java/com/example/training/MainActivity.java
@@ -164,7 +164,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
         if(data!=null) {
             mJsonString = data.getStringExtra("newJsonString");
--- app/src/test/java/com/example/training/ExampleUnitTest.java
+++ app/src/test/java/com/example/training/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.training;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,8 +12,30 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void javaTest(){
+        int first = 5;
+        int second = 10;
+
+        mathTest math = new mathTest(first, second);
+        System.out.println("Answer: " + math.sum());
     }
+
+}
+
+class mathTest{
+
+    private int firstNumber;
+    private int secondNumber;
+
+    public mathTest(int firstNumber, int secondNumber){
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public int sum(){
+        return firstNumber + secondNumber;
+    }
+
 }
