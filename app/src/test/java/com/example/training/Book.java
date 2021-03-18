765a1796822315f6126b9f6fcae6a1697b99372a
turismo53 <turismo53@gmail.com>
2021-03-18 15:08:59 +0200

    java interface

---
+++ app/src/test/java/com/example/training/Book.java
@@ -0,0 +1,20 @@
+package com.example.training;
+import org.junit.Test;
+
+
+public class Book implements Printable{
+
+    private String mTitle;
+    private String mAuthor;
+
+    public Book(){
+        mTitle = "Random title";
+        mAuthor = "Random author";
+    }
+
+    @Test
+    public void print(){
+        System.out.println("Title: " + mTitle + "\nAuthor: " + mAuthor);
+    }
+
+}
---
+++ app/src/test/java/com/example/training/Printable.java
@@ -0,0 +1,5 @@
+package com.example.training;
+
+public interface Printable {
+    void print();
+}
