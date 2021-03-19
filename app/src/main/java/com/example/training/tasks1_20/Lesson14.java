f985213e2c82c1b381263727a8e6ed900ee823d1
turismo53 <turismo53@gmail.com>
2021-03-19 23:11:27 +0200

    lesson 14

---
+++ app/src/main/java/com/example/training/tasks1_20/Lesson14.java
@@ -0,0 +1,50 @@
+package com.example.training.tasks1_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.training.R;
+
+public class Lesson14 extends AppCompatActivity {
+
+    TextView mTv;
+    CheckBox mChb;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson14);
+        mTv = (TextView) findViewById(R.id.textView);
+        mChb = (CheckBox) findViewById(R.id.chbExtMenu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.setGroupVisible(R.id.group1, mChb.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        mTv.setText(sb.toString());
+
+        return super.onOptionsItemSelected(item);
+    }
+}
