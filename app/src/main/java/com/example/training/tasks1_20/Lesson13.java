dda6c6d0fde98d09d2b284f44a7929d63dd4f507
turismo53 <turismo53@gmail.com>
2021-03-19 22:36:48 +0200

    lesson 13

---
+++ app/src/main/java/com/example/training/tasks1_20/Lesson13.java
@@ -0,0 +1,32 @@
+package com.example.training.tasks1_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.training.R;
+
+public class Lesson13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson13);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("menu1");
+        menu.add("menu2");
+        menu.add("menu3");
+        menu.add("menu4");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
