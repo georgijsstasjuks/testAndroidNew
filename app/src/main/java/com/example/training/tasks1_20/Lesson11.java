ad22428fa136de70f1e965790be2a03cbba97aff
turismo53 <turismo53@gmail.com>
2021-03-19 21:09:27 +0200

    lesson 11

---
+++ app/src/main/java/com/example/training/tasks1_20/Lesson11.java
@@ -0,0 +1,26 @@
+package com.example.training.tasks1_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.training.R;
+
+public class Lesson11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson11);
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+    }
+}
