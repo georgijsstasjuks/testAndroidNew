a9622e47ae4dcd481295964584209a55dbc23b6e
turismo53 <turismo53@gmail.com>
2021-03-25 01:23:14 +0200

    lesson 33

---
+++ app/src/main/java/com/example/training/third/SharedPreferencesActivity.java
@@ -0,0 +1,72 @@
+package com.example.training.third;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.training.R;
+
+public class SharedPreferencesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEtText;
+    private Button mBtnSave, mBtnLoad;
+    private SharedPreferences mSpref;
+
+    final String SAVED_TEXT = "saved_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preferences_activity);
+
+        mEtText = (EditText) findViewById(R.id.etText);
+
+        mBtnSave = (Button) findViewById(R.id.btnSave);
+        mBtnSave.setOnClickListener(this);
+
+        mBtnLoad = (Button) findViewById(R.id.btnLoad);
+        mBtnLoad.setOnClickListener(this);
+        loadText();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
+    }
+
+   private void saveText() {
+        mSpref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = mSpref.edit();
+        ed.putString(SAVED_TEXT, mEtText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadText() {
+        mSpref = getPreferences(MODE_PRIVATE);
+        String savedText = mSpref.getString(SAVED_TEXT, "");
+        mEtText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+    }
+
+}
