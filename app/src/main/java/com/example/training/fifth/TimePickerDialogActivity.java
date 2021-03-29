512809b51cc44300c18a4ad76a924a558a59654d
turismo53 <turismo53@gmail.com>
2021-03-29 16:18:07 +0300

    lesson 58

---
+++ app/src/main/java/com/example/training/fifth/TimePickerDialogActivity.java
@@ -0,0 +1,48 @@
+package com.example.training.fifth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.example.training.R;
+
+public class TimePickerDialogActivity extends AppCompatActivity {
+
+    private final static int DIALOG_TIME = 1;
+    private int myHour = 14;
+    private int myMinute = 35;
+    private TextView mTvTime;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker_dialog);
+        mTvTime = (TextView) findViewById(R.id.tvTime);
+    }
+
+    public void onclick(View view) {
+        showDialog(DIALOG_TIME);
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            mTvTime.setText("Time is " + myHour + " hours " + myMinute + " minutes");
+        }
+    };
+
+
+}
