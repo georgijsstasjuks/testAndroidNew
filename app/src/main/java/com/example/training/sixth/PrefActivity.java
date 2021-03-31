e0f093c8499f3e2dc01d2c5b70255c38076426bf
turismo53 <turismo53@gmail.com>
2021-03-31 14:13:34 +0300

    lesson 73

--- app/src/main/java/com/example/training/sixth/PrefActivity.java
+++ app/src/main/java/com/example/training/sixth/PrefActivity.java
@@ -1,14 +1,29 @@
 package com.example.training.sixth;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 
 import com.example.training.R;
 
 public class PrefActivity extends PreferenceActivity {
 
+    private CheckBoxPreference mChb3;
+    private PreferenceCategory mCateg2;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref);
+
+        mChb3 = (CheckBoxPreference) findPreference("chb3");
+        mCateg2  = (PreferenceCategory) findPreference("categ2");
+        mCateg2.setEnabled(mChb3.isChecked());
+
+        mChb3.setOnPreferenceClickListener(preference -> {
+            mCateg2.setEnabled(mChb3.isChecked());
+            return false;
+        });
     }
 }
---
+++ app/src/main/java/com/example/training/sixth/PreferencesEnableActivity.java
@@ -0,0 +1,24 @@
+package com.example.training.sixth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.training.R;
+
+public class PreferencesEnableActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences_enable);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
