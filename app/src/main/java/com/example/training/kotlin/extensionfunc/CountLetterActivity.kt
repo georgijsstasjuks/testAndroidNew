effcd0005b8a684a2ea2a91a61516d69d08cfdda
turismo53 <turismo53@gmail.com>
2021-05-14 17:38:23 +0300

    kotlin extension function

---
+++ app/src/main/java/com/example/training/kotlin/extensionfunc/CountLetterActivity.kt
@@ -0,0 +1,39 @@
+package com.example.training.kotlin.extensionfunc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.training.R
+import kotlinx.android.synthetic.main.activity_count_letter.*
+
+class CountLetterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_count_letter)
+        setupListeners()
+    }
+
+    fun setupListeners() {
+        btSendInt.setOnClickListener {
+            val number = etInt.text.toString()
+            tvAnswer.text = number.toInt().square().toString()
+        }
+
+        btSendString.setOnClickListener {
+            val word = etString.text.toString()
+            val letter = etLetter.text
+            tvAnswer.text = word.countLetter(letter.toString().toCharArray()).toString()
+        }
+    }
+
+    fun String.countLetter(c: CharArray) : Int{
+        var count = 0
+        for (n in 0 until this.count()) {
+            if(this[n].toLowerCase() == c[0].toLowerCase()) count++
+        }
+        return count
+    }
+
+    fun Int.square(): Int { return this * this }
+
+}
